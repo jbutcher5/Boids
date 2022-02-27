e182241eabf0a0a2a968b0ea8514df2ace70ae65
James Butcher <jamesbutcher167@gmail.com>
2022-02-27 17:43:01 +0000

    Remove shell files

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "INFO: Deleting existing build"
-rm -rf ./build/
-echo "INFO: Copying src to build"
-cp -r ./src ./build
-echo "INFO: Compiling source code"
-clang ./build/*.c -lraylib -o ./boids
--- debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-./build.sh
-/bin/gdb ./boids 
