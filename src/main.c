2b8d484c106b5993d622112fe0d9b8f6a76118b1
James Butcher <jamesbutcher167@gmail.com>
2022-03-03 19:35:26 +0000

    Increase number of boids

--- src/main.c
+++ src/main.c
@@ -13,19 +13,19 @@ int main(void) {
 	rlDisableBackfaceCulling();
 	SetTargetFPS(FPS);
 
-	Boid* flock[32];
+	Boid* flock[128];
 
-	for (int i = 0; i < 32; i++)
-		flock[i] = newBoid((Vector2){GetRandomValue(0, WIDTH), GetRandomValue(i, HEIGHT)}, (Vector2){20, 20});
+	for (int i = 0; i < 128; i++)
+		flock[i] = newBoid((Vector2){GetRandomValue(0, WIDTH), GetRandomValue(i, HEIGHT)}, (Vector2){20, 20}, 0);
 
 	while (!WindowShouldClose()){
-		for (int i = 0; i < 32; i++)
-			updateBoid(flock[i], flock, 32);
+		for (int i = 0; i < 128; i++)
+			updateBoid(flock[i], flock, 128);
 
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 
-		for (int i = 0; i < 32; i++)
+		for (int i = 0; i < 128; i++)
 			drawBoid(flock[i]);
 
 		EndDrawing();
