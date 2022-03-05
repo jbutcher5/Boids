9612dfc5404f9359e0f5c2f2f26068a41511399c
James Butcher <jamesbutcher167@gmail.com>
2022-03-05 23:37:00 +0000

    Find optimal parameters

--- src/main.c
+++ src/main.c
@@ -7,25 +7,26 @@
 #define WIDTH 800
 #define HEIGHT 450
 #define TITLE "Boids Example"
+#define BOIDS 128
 
 int main(void) {
 	InitWindow(WIDTH, HEIGHT, TITLE);
 	rlDisableBackfaceCulling();
 	SetTargetFPS(FPS);
 
-	Boid* flock[256];
+	Boid* flock[BOIDS];
 
-	for (int i = 0; i < 256; i++)
-		flock[i] = newBoid((Vector2){GetRandomValue(0, WIDTH), GetRandomValue(i, HEIGHT)}, (Vector2){20, 20}, 0, 50);
+	for (int i = 0; i < BOIDS; i++)
+		flock[i] = newBoid((Vector2){GetRandomValue(0, WIDTH), GetRandomValue(i, HEIGHT)}, (Vector2){20, 20}, GetRandomValue(0, 6), .1);
 
 	while (!WindowShouldClose()){
-		for (int i = 0; i < 256; i++)
-			updateBoid(flock[i], flock, 256);
+		for (int i = 0; i < BOIDS; i++)
+			updateBoid(flock[i], flock, BOIDS);
 
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 
-		for (int i = 0; i < 256; i++)
+		for (int i = 0; i < BOIDS; i++)
 			drawBoid(flock[i]);
 
 		EndDrawing();
